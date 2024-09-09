75a93dfcfc4bbe888f9d20339c11c443351413de
Jeferson Alan <cabello_10@live.com>
2024-09-09 20:58:21 -0300

    Update MODELO FÍSICO - rede de academias.sql

--- MODELO FÍSICO - rede de academias.sql
+++ MODELO FÍSICO - rede de academias.sql
@@ -7,20 +7,8 @@ id INT NOT NULL AUTO_INCREMENT,
 nome VARCHAR(45) NOT NULL,
 endereco VARCHAR(45) NOT NULL,
 telefone VARCHAR(15) NOT NULL, 
-id_aluno INT NOT NULL,
-PRIMARY KEY(id),
-FOREIGN KEY (id_aluno) REFERENCES alunos(id) ); 
-
-CREATE TABLE IF NOT EXISTS alunos (
-id INT NOT NULL AUTO_INCREMENT,
-nome VARCHAR(45) NOT NULL,
-endereco VARCHAR(45) NOT NULL,
-telefone VARCHAR(15) NOT NULL, 
-id_plano INT NOT NULL,
-id_acesso INT NOT NULL,
-PRIMARY KEY(id),
-FOREIGN KEY (id_plano) REFERENCES plano(id) ,
-FOREIGN KEY (id_acesso) REFERENCES acesso(id) );
+PRIMARY KEY(id)
+); 
 
 CREATE TABLE IF NOT EXISTS plano (
 id INT NOT NULL AUTO_INCREMENT,
@@ -28,9 +16,35 @@ tipo_plano VARCHAR(10) NOT NULL,
 PRIMARY KEY(id)
 );
 
+CREATE TABLE IF NOT EXISTS alunos (
+id INT NOT NULL AUTO_INCREMENT,
+nome VARCHAR(45) NOT NULL,
+endereco VARCHAR(45) NOT NULL,
+telefone VARCHAR(15) NOT NULL,
+cpf VARCHAR(14) NOT NULL,  
+data_nasc DATE NOT NULL,
+email VARCHAR (30) NOT NULL,
+id_plano INT NOT NULL,
+PRIMARY KEY(id),
+FOREIGN KEY (id_plano) REFERENCES plano(id)
+);
+
+CREATE TABLE IF NOT EXISTS matricula (
+id INT NOT NULL AUTO_INCREMENT,
+data_matricula DATETIME NOT NULL,
+id_aluno INT NOT NULL,
+id_unidade INT NOT NULL,
+PRIMARY KEY(id),
+FOREIGN KEY (id_aluno) REFERENCES alunos(id),
+FOREIGN KEY (id_unidade) REFERENCES unidade(id)
+ );
+ 
 CREATE TABLE IF NOT EXISTS acesso (
 id INT NOT NULL AUTO_INCREMENT,
-tipo_acesso VARCHAR(15) NOT NULL, 
+data_acesso DATETIME NOT NULL,
 id_aluno INT NOT NULL,
-PRIMARY KEY(id)
+id_unidade INT NOT NULL,
+PRIMARY KEY(id),
+FOREIGN KEY (id_aluno) REFERENCES alunos(id),
+FOREIGN KEY (id_unidade) REFERENCES unidade(id)
  );
