2c7ee754d64a8d2df2592bbf121c128a9a88f46d
Jeferson Alan <cabello_10@live.com>
2024-09-11 20:55:05 -0300

    Update MODELO FÍSICO - rede de academias INSERCOES.sql

--- MODELO FÍSICO - rede de academias INSERCOES.sql
+++ MODELO FÍSICO - rede de academias INSERCOES.sql
@@ -21,37 +21,56 @@ INSERT INTO plano (tipo_plano)
 			('Basico');
             
 INSERT INTO alunos (nome, endereco, telefone, cpf, data_nasc, email, id_plano)
-	VALUES ('Jeferson Alan','Q11 Cj 02','61-9-9283-6947','043.383.601-59','1994-08-01','alan@alan','1'),
-		('Maria Teixeira','Q10 Cj 04','61-9-8147-6943','044.383.601-60','1993-07-01','maria@alan','2'),
-            	('Cristiano Ronaldo','Q09 Cj 08','61-9-9955-6945','055.383.601-57','1992-06-01','cris@alan','1'),
-            	('Marie Jane','Q07 Cj 01','61-9-8852-6984','061.383.601-56','1991-05-01','marie@alan','2'),
-            	('Ana Maria','Q24 Cj 12','61-9-9128-6969','032.383.601-55','1990-10-01','ana@alan','2'),
-            	('Jessica Alba','Q11 Cj 06','61-9-9998-6923','021.383.601-54','1989-11-01','jess@alan','1'),
-            	('Marilia Mendonca','Q09 Cj 02','61-9-9149-6921','043.383.601-53','1994-03-01','mendonca@alan','2'),
-            	('Maiara Souza','Q03 Cj 02','61-9-9249-6790','041.383.601-52','1993-12-01','mamai@alan','2'),
-            	('Maraisa Souza','Q01 Cj 02','61-9-9255-6847','084.383.601-51','1992-08-01','mara@alan','2'),
-            	('Patricia Poeta','Q08 Cj 10','61-9-9281-6587','099.383.601-50','1991-08-01','paty@alan','1');
+	 VALUES ('Jeferson Alan','Q11 Cj 02','61-9-9283-6947','043.383.601-59','1994-08-01','alan@alan','1'),
+			('Maria Teixeira','Q10 Cj 04','61-9-8147-6943','044.383.601-60','1993-07-01','maria@alan','2'),
+            ('Cristiano Ronaldo','Q09 Cj 08','61-9-9955-6945','055.383.601-57','1992-06-01','cris@alan','1'),
+            ('Marie Jane','Q07 Cj 01','61-9-8852-6984','061.383.601-56','1991-05-01','marie@alan','2'),
+            ('Ana Maria','Q24 Cj 12','61-9-9128-6969','032.383.601-55','1990-10-01','ana@alan','2'),
+            ('Jessica Alba','Q11 Cj 06','61-9-9998-6923','021.383.601-54','1989-11-01','jess@alan','1'),
+            ('Marilia Mendonca','Q09 Cj 02','61-9-9149-6921','043.383.601-53','1994-03-01','mendonca@alan','2'),
+            ('Maiara Souza','Q03 Cj 02','61-9-9249-6790','041.383.601-52','1993-12-01','mamai@alan','2'),
+            ('Maraisa Souza','Q01 Cj 02','61-9-9255-6847','084.383.601-51','1992-08-01','mara@alan','2'),
+            ('Patricia Poeta','Q08 Cj 10','61-9-9281-6587','099.383.601-50','1991-08-01','paty@alan','1');
             
 INSERT INTO matricula (data_matricula, id_aluno, id_unidade)
 	 VALUES ('2024-01-13','1','6'),
-		('2024-03-17','2','5'),
-		('2024-02-19','3','4'),
-            	('2024-04-24','4','3'),
-            	('2024-06-27','5','2'),
-            	('2024-05-03','6','1'),
-            	('2024-07-07','7','6'),
-            	('2024-04-13','8','5'),
-            	('2024-08-15','9','4'),
-            	('2024-05-20','10','3');
+			('2024-03-17','2','5'),
+			('2024-02-19','3','4'),
+            ('2024-04-24','4','3'),
+            ('2024-06-27','5','2'),
+            ('2024-05-03','6','1'),
+            ('2024-07-07','7','6'),
+            ('2024-04-13','8','5'),
+            ('2024-08-15','9','4'),
+            ('2024-05-20','10','3');
 
 INSERT INTO acesso (data_acesso, id_aluno, id_unidade)
-	VALUES ('2024-09-10','1','6'),
-		('2024-09-10','2','5'),
-                ('2024-09-10','3','4'),
-                ('2024-09-10','4','3'),
-                ('2024-09-10','5','2'),
-                ('2024-09-10','6','1'),
-                ('2024-09-10','7','6'),
-                ('2024-09-10','8','5'),
-                ('2024-09-10','9','4'),
-                ('2024-09-10','10','3');
+		 VALUES ('2024-09-11','1','6'),
+				('2024-09-11','2','5'),
+                ('2024-09-11','3','4'),
+                ('2024-09-11','4','3'),
+                ('2024-09-11','5','2'),
+                ('2024-09-11','6','1'),
+                ('2024-09-11','7','6'),
+                ('2024-09-11','8','5'),
+                ('2024-09-11','9','4'),
+                ('2024-09-11','10','3');
+-- -----------------------------------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------------------------------
+                
+-- CRIANDO ACESSOS ALEATORIOS
+INSERT INTO acesso (data_acesso, id_aluno, id_unidade)
+SELECT 
+    CONCAT(
+        DATE_ADD('2024-01-01', INTERVAL FLOOR(RAND() * 243) DAY), 
+        ' ', 
+        SEC_TO_TIME(FLOOR(8 * 3600 + RAND() * (15 * 3600)))
+    ) AS data_acesso,
+    FLOOR(1 + RAND() * 10) AS id_aluno,
+    FLOOR(1 + RAND() * 6) AS id_unidade
+FROM 
+    (SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5 UNION ALL SELECT 6 UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9 UNION ALL SELECT 10) AS numbers1,
+    (SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5 UNION ALL SELECT 6 UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9 UNION ALL SELECT 10) AS numbers2
+LIMIT 300;
+
+SELECT count(*) FROM acesso
