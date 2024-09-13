56e9e57f0239cbdb7434054dd2c610b5c8113178
Jeferson Alan <cabello_10@live.com>
2024-09-13 16:35:31 -0300

    Update MODELO FISICO - exercicios 3.sql

--- MODELO FISICO - exercicios 3.sql
+++ MODELO FISICO - exercicios 3.sql
@@ -26,8 +26,30 @@ SELECT alunos.nome, DATEDIFF(curdate(), MAX(matricula.data_matricula)) AS difere
     GROUP BY alunos.nome ;
 
 -- 4 Quais unidades foram adicionadas após 1º de setembro de 2024?
+-- ?
+
 -- 5 Qual foi a primeira data de matrícula para cada unidade?
+SELECT unidade.nome, MIN(data_matricula) FROM matricula
+	JOIN unidade ON unidade.id = matricula.id_unidade
+    GROUP BY unidade.nome;
+
 -- 6 Quais alunos possuem acessos em mais de uma unidade e qual foi o primeiro acesso registrado?
+SELECT
+    a.id,
+    a.nome,
+    MIN(ac.data_acesso) AS primeiro_acesso
+FROM
+    alunos a
+JOIN
+    acesso ac ON a.id = ac.id_aluno
+JOIN
+    unidade u ON ac.id_unidade = u.id
+GROUP BY
+    a.id, a.nome
+HAVING
+    COUNT(DISTINCT ac.id_unidade) > 1;
+
+    
 -- 7 Quantos dias desde a última matrícula até a data atual para o aluno com CPF '12345678901'?
 -- 8 Qual a data de matrícula do aluno que foi matriculado na unidade com nome 'Academia Norte'?
 -- 9 Quais unidades não tiveram matrículas registradas no último mês?
