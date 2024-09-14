2de8404013e58b2811d7fa93ba13c5a9b36c3d2a
Jeferson Alan <cabello_10@live.com>
2024-09-13 21:07:56 -0300

    Update MODELO FISICO - exercicios 3.sql

--- MODELO FISICO - exercicios 3.sql
+++ MODELO FISICO - exercicios 3.sql
@@ -34,23 +34,34 @@ SELECT unidade.nome, MIN(data_matricula) FROM matricula
     GROUP BY unidade.nome;
 
 -- 6 Quais alunos possuem acessos em mais de uma unidade e qual foi o primeiro acesso registrado?
-SELECT
-    a.id,
-    a.nome,
-    MIN(ac.data_acesso) AS primeiro_acesso
-FROM
-    alunos a
-JOIN
-    acesso ac ON a.id = ac.id_aluno
-JOIN
-    unidade u ON ac.id_unidade = u.id
-GROUP BY
-    a.id, a.nome
-HAVING
-    COUNT(DISTINCT ac.id_unidade) > 1;
+SELECT a.id, a.nome, MIN(ac.data_acesso) AS primeiro_acesso FROM alunos a
+	JOIN acesso ac ON a.id = ac.id_aluno
+	JOIN unidade u ON ac.id_unidade = u.id
+		GROUP BY a.id, a.nome
+			HAVING COUNT(DISTINCT ac.id_unidade) > 1; -- DISTINCT nao traz valores dubplicados
 
-    
--- 7 Quantos dias desde a última matrícula até a data atual para o aluno com CPF '12345678901'?
--- 8 Qual a data de matrícula do aluno que foi matriculado na unidade com nome 'Academia Norte'?
+-- 7 Quantos dias desde a última matrícula até a data atual para o aluno com CPF ''043.383.601-59''
+
+SELECT alunos.nome, alunos.cpf, DATEDIFF(CURDATE(), MAX(matricula.data_matricula)) AS dias_desde_ultima_matricula FROM matricula
+	INNER JOIN alunos ON alunos.id = matricula.id_aluno
+	WHERE alunos.cpf = '043.383.601-59'
+		GROUP BY alunos.nome, alunos.cpf;
+
+-- 8 Qual a data de matrícula do aluno que foi matriculado na unidade com nome 'Asa Norte'?
+SELECT alunos.nome, matricula.data_matricula FROM alunos
+	JOIN matricula ON alunos.id = matricula.id_aluno
+	JOIN unidade ON matricula.id_unidade = unidade.id
+	WHERE unidade.nome = 'Asa Norte';
+
 -- 9 Quais unidades não tiveram matrículas registradas no último mês?
+SELECT unidade.nome FROM unidade
+	LEFT JOIN matricula ON unidade.id = matricula.id_unidade
+		AND matricula.data_matricula >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH)
+		AND matricula.data_matricula < CURDATE()
+	WHERE matricula.id_unidade IS NULL;
+
 -- 10 Qual é a diferença em dias entre o primeiro e o último acesso de cada aluno?
+SELECT alunos.nome, DATEDIFF(MAX(acesso.data_acesso), MIN(acesso.data_acesso)) AS diferenca_dias
+	FROM alunos
+	JOIN acesso ON alunos.id = acesso.id_aluno
+GROUP BY alunos.id, alunos.nome;
